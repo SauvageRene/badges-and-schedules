ee7138f629eff27656db60422d03757910fca39b
Rene <renegarcia1864@gmail.com>
2021-02-03 17:31:45 -0600

    commit tree 57ba249ce36858e4dcbde410554c87d89057eba7

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,34 +1,31 @@
 # Write your code here.
-
 def badge_maker(name)
-    name = "Arel"
-     "Hello, my name is #{name}."
-end
-def batch_badge_creator(array)
-    
-    new_array = []
-    array.each do |badges|
-        new_array.push("Hello, my name is #{badges}.")
-    end
-    return new_array
-end
-#for badge creator using method was not working, only listing 
-def assign_rooms(rooms)
-    new_rooms = []
-    counter = 1
-    rooms.each do |badges|
-        new_rooms.push("Hello, #{badges}! You'll be assigned to room #{counter}!")
-        counter+=1
-    end
-        return new_rooms
+ "Hello, my name is #{name}."
 end
 
-def printer(namesandrooms)
-    batch_badge_creator(namesandrooms).each do |badges|
-        puts badges 
+def batch_badge_creator(attendees)
+    badges=[]
+    attendees.each do |badge|
+        badges.push("Hello, my name is #{badge}.")
     end
-    assign_rooms(namesandrooms).each do |rooms|
+    badges
+end
+
+def assign_rooms(attendees)
+    new_array = []
+    counter = 1
+    attendees.each do |attendee| 
+        new_array << "Hello, #{attendee}! You'll be assigned to room #{counter}!"
+        counter += 1
+    end
+    new_array
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badges|
+        puts badges
+    end
+    assign_rooms(attendees).each do |rooms|
         puts rooms
     end
-
 end
